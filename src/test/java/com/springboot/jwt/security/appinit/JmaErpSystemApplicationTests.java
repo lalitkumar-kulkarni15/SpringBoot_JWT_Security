6590314fc9b0b096c1ed275b59454dd23ddebebb
lalit <lalitkulkarniofficial@gmail.com>
2018-10-07 23:54:13 +0530

    Refactoring the package structure

--- src/test/java/com/springboot/jwt/security/appinit/JmaErpSystemApplicationTests.java
+++ src/test/java/com/springboot/jwt/security/appinit/JmaErpSystemApplicationTests.java
@@ -1,12 +1,12 @@
-package com.jma.main;
+package com.springboot.jwt.security.appinit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JmaErpSystemApplicationTests {
 
 	@Test
